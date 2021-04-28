9d210fd471e563dff5f218fd3d7acec360ae76ec
CesarArellano <raywayday@gmail.com>
2021-04-28 11:54:48 -0500

    Utilizando TextToSpeach en la app

--- Hello Kotlin/app/src/main/java/com/cesararellano/hellokotlin/MainActivity.kt
+++ Hello Kotlin/app/src/main/java/com/cesararellano/hellokotlin/MainActivity.kt
@@ -4,6 +4,7 @@ import kotlinx.android.synthetic.main.activity_main.*
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.speech.tts.TextToSpeech
+import java.util.*
 
 class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
 
@@ -13,11 +14,18 @@ class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         tts = TextToSpeech(this, this)
+        btnPlay.setOnClickListener{speak()}
+    }
+
+    private fun speak() {
+        var message: String = textView.text.toString()
+        tts!!.speak(message, TextToSpeech.QUEUE_FLUSH, null, "")
     }
 
     override fun onInit(status: Int) {
         if (status == TextToSpeech.SUCCESS) {
-            textView.text = "Listo!"
+            textView.text = "Hola, soy Kotlin"
+            tts!!.setLanguage(Locale("ES"))
         } else {
             textView.text = "No disponible!"
         }
