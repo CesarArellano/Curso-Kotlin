21b8e1678425d63dea32152f61d2b4ff5088e8f8
CesarArellano <raywayday@gmail.com>
2021-05-02 22:18:11 -0500

    Terminando Sección 2

--- Hello Kotlin/app/src/main/java/com/cesararellano/hellokotlin/MainActivity.kt
+++ Hello Kotlin/app/src/main/java/com/cesararellano/hellokotlin/MainActivity.kt
@@ -20,15 +20,27 @@ class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
 
     private fun speak() {
         var message: String = binding.editMessage.text.toString()
+        if(message.isEmpty()) {
+            binding.txtStatus.text = "Introduzca un texto"
+            message = "¿Es en serio? Ya pon algo en el Edit Text!"
+        }
         tts!!.speak(message, TextToSpeech.QUEUE_FLUSH, null, "")
     }
 
     override fun onInit(status: Int) {
         if (status == TextToSpeech.SUCCESS) {
-            binding.txtStatus.text = "Hola, soy Kotlin"
+            binding.txtStatus.text = "Listo"
             tts!!.language = Locale("ES")
         } else {
             binding.txtStatus.text = "No disponible!"
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        if (tts != null) {
+            tts!!.stop()
+            tts!!.shutdown()
         }
     }
 }
