be14ad8ad390088ae6ae93672a8174ab58eb8496
CesarArellano <raywayday@gmail.com>
2021-05-02 22:04:51 -0500

    Mejoras Visuales

--- Hello Kotlin/app/src/main/java/com/cesararellano/hellokotlin/MainActivity.kt
+++ Hello Kotlin/app/src/main/java/com/cesararellano/hellokotlin/MainActivity.kt
@@ -1,33 +1,34 @@
 package com.cesararellano.hellokotlin
-import kotlinx.android.synthetic.main.activity_main.*
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.speech.tts.TextToSpeech
+import com.cesararellano.hellokotlin.databinding.ActivityMainBinding
 import java.util.*
 
 class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
-
+    private lateinit var binding: ActivityMainBinding
     var tts: TextToSpeech? = null;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         tts = TextToSpeech(this, this)
-        btnPlay.setOnClickListener{speak()}
+        binding.btnPlay.setOnClickListener{speak()}
     }
 
     private fun speak() {
-        var message: String = textView.text.toString()
+        var message: String = binding.txtStatus.text.toString()
         tts!!.speak(message, TextToSpeech.QUEUE_FLUSH, null, "")
     }
 
     override fun onInit(status: Int) {
         if (status == TextToSpeech.SUCCESS) {
-            textView.text = "Hola, soy Kotlin"
-            tts!!.setLanguage(Locale("ES"))
+            binding.txtStatus.text = "Hola, soy Kotlin"
+            tts!!.language = Locale("ES")
         } else {
-            textView.text = "No disponible!"
+            binding.txtStatus.text = "No disponible!"
         }
     }
 }
