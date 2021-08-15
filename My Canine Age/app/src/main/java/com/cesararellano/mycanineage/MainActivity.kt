ab08e0cd82bcbf161f5a1f780a987709f57676c9
Cesar Arellano <cesar.arellano@gtec.com.mx>
2021-08-14 23:04:09 -0500

    Updating repo

---
+++ My Canine Age/app/src/main/java/com/cesararellano/mycanineage/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.cesararellano.mycanineage
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
