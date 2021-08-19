4562b249454f5409bdbef5cd2f338bb347023920
Cesar Arellano <cesar.arellano@gtec.com.mx>
2021-08-18 19:58:33 -0500

    Counter App Completed

--- CalculatorApp/app/src/main/java/com/cesararellano/calculatorapp/MainActivity.kt
+++ CalculatorApp/app/src/main/java/com/cesararellano/calculatorapp/MainActivity.kt
@@ -3,18 +3,43 @@ package com.cesararellano.calculatorapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Button
+import android.widget.TextView
 
 private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var firstButton: Button
+    private lateinit var incrementButton: Button
+    private lateinit var resetButton: Button
+    private lateinit var display: TextView
+    private var counter: Int = 0
+
     override fun onCreate( savedInstanceState: Bundle? ) {
         super.onCreate(savedInstanceState)
         setContentView( R.layout.activity_main )
-        firstButton = findViewById( R.id.firstButton )
-        firstButton.setOnClickListener {
-            Log.d(TAG, "Pressing mi firstButton")
+
+        display = findViewById( R.id.display)
+        incrementButton = findViewById( R.id.firstButton )
+        resetButton = findViewById( R.id.resetButton )
+
+        incrementButton.setOnClickListener {
+            Log.d(TAG, "Pressing accept")
+            counter++
+            display.text = counter.toString()
+        }
+        resetButton.setOnClickListener {
+            counter = 0
+            display.text = counter.toString()
+        }
+    }
+
+    @Suppress( "UNUSED_PARAMETER")
+    fun onPressedButton(button: View) {
+        Log.d(TAG, "Pressing cancel")
+        if( counter > 0) {
+            counter--
+            display.text = counter.toString()
         }
     }
 }
