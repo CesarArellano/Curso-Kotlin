0dba68382266503312b3f88d72e6f9d98e845096
Cesar Arellano <cesar.arellano@gtec.com.mx>
2021-08-25 19:54:37 -0500

    Refactoring code

--- CalculatorApp/app/src/main/java/com/cesararellano/calculatorapp/MainActivity.kt
+++ CalculatorApp/app/src/main/java/com/cesararellano/calculatorapp/MainActivity.kt
@@ -1,108 +1,62 @@
 package com.cesararellano.calculatorapp
 
-import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-// import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
+// import android.util.Log
 
 //private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var display: TextView
-    private var usuarioEstaEscribiendoNumero = false
-    private var resultado = 0
-    private val modeloCalculadora = ModeloCalculadora()
-    /*
-    private lateinit var incrementButton: Button
-    private lateinit var resetButton: Button
 
-    private var counter: Int = 0
-    */
+    private lateinit var display: TextView
+    private val modeloCalculadora = ModeloCalculadora()
+
     override fun onCreate( savedInstanceState: Bundle? ) {
         super.onCreate(savedInstanceState)
         setContentView( R.layout.activity_main )
 
         display = findViewById( R.id.display)
-        /*
-        display = findViewById( R.id.display)
-        incrementButton = findViewById( R.id.firstButton )
-        resetButton = findViewById( R.id.resetButton )
-
-        incrementButton.setOnClickListener {
-            Log.d(TAG, "Pressing accept")
-            counter++
-            display.text = counter.toString()
-        }
-        resetButton.setOnClickListener {
-            counter = 0
-            display.text = counter.toString()
-        }
-        */
     }
 
     @Suppress( "UNUSED_PARAMETER")
-    fun onPressedButton(button: View) {
-        val digito = (button as Button).text
-        if( usuarioEstaEscribiendoNumero ) {
-            if(display.text.toString() == "0") {
-                display.text = digito
-            } else {
-                display.append(digito)
-            }
-
-        } else {
-            display.text = digito
-            usuarioEstaEscribiendoNumero = true
-        }
-
+    fun agregarNumeroCtrl(boton: View) {
+        var number = (boton as Button).text.toString()
+        if( number == "𝛑") number = "3.1416"
+        display.text = modeloCalculadora.agregarNumero(number)
     }
 
     @Suppress( "UNUSED_PARAMETER")
-    fun addDecimalPoint(botton: View) {
-        if ( display.text.contains(".") ) return
-
-        if ( display.text.startsWith("0" )) {
-            display.text = "0."
-        } else {
-            display.text = ( display.text.toString() + "." )
-        }
+    fun agregarPuntoDecimalCtrl(boton: View) {
+        display.text = modeloCalculadora.agregarPuntoDecimal()
     }
 
     @Suppress( "UNUSED_PARAMETER")
-    fun changeNegativePositive(botton: View) {
-        if (display.text.startsWith("-")) {
-            display.text = display.text.toString().replace("-", "")
-        } else {
-            display.text = ( "-" + display.text.toString() )
-        }
+    fun cambiarSignoCtrl(boton: View) {
+        display.text = modeloCalculadora.cambiarSigno()
     }
 
     @Suppress( "UNUSED_PARAMETER")
-    fun deleteLastEntry(botton: View) {
-        if ( display.text.toString().replace("-", "").length > 1 ) {
-            display.text = display.text.substring(0, display.text.length - 1)
-        } else {
-            display.text = "0"
-        }
+    fun eliminarUltimaEntradaCtrl(boton: View) {
+        display.text = modeloCalculadora.eliminarUltimaEntrada()
     }
 
     @Suppress( "UNUSED_PARAMETER")
-    fun resetAll(botton: View) {
+    fun resetearCtrl(boton: View) {
+        modeloCalculadora.resetear()
         display.text = "0"
-        modeloCalculadora.setZero()
     }
 
-    fun operacionPresionado(botton: View) {
-        val operacionPresionada = (botton as Button).text.toString()
-        if( usuarioEstaEscribiendoNumero ) {
-            modeloCalculadora.setOperando( display.text.toString().toInt() )
-            usuarioEstaEscribiendoNumero = false
-        }
-        resultado = modeloCalculadora.ejecutaOperacion(operacionPresionada)
-        display.text = resultado.toString()
+    fun operadorSeleccionadoCtrl(boton: View) {
+        val operador = (boton as Button).text.toString()
+        display.text = modeloCalculadora.operadorSeleccionado(operador)
+    }
+
+    @Suppress("UNUSED_PARAMETER")
+    fun calcularResultadoCtrl(botton: View) {
+        display.text = modeloCalculadora.calcularResultado()
     }
 
 }
--- CalculatorApp/app/src/main/java/com/cesararellano/calculatorapp/ModeloCalculadora.kt
+++ CalculatorApp/app/src/main/java/com/cesararellano/calculatorapp/ModeloCalculadora.kt
@@ -1,38 +1,91 @@
 package com.cesararellano.calculatorapp
 
 class ModeloCalculadora {
-    private var operandoEnEspera = 0 // Primer operando
-    private var operando = 0
-    private var operacionEnEsperaDeOperando = ""
+    private var operandoEnEspera:String = "0" // Primer operando
+    private var operando:String = "0"
+    private var operacionEnEsperaDeOperando = "+"
+    private var resultado:String = "0"
 
-    fun setZero() {
-        operandoEnEspera = 0
-        operando = 0
-        operacionEnEsperaDeOperando = ""
+    fun resetear() {
+        operandoEnEspera = "0"
+        operando = "0"
+        operacionEnEsperaDeOperando = "+"
+        resultado = "0"
     }
 
-    fun setOperando(unOperando: Int) {
-        operando = unOperando
+    fun agregarNumero(number: String): String {
+        if (resultado == "0") {
+            resultado = number
+            return resultado
+        }
+
+        if (resultado == "-0") {
+            resultado = "-$number"
+            return resultado
+        }
+        resultado += number
+        return resultado
     }
 
-    fun ejecutaOperacion(operacion: String): Int {
-        ejecutaOperacionEnEspera()
-        operacionEnEsperaDeOperando = operacion
-        operandoEnEspera = operando
-        return operando
+    fun agregarPuntoDecimal():String {
+        if (resultado.contains('.')) return resultado
+
+        resultado = if (resultado.startsWith("0")) {
+            "0."
+        } else {
+            "$resultado."
+        }
+
+        return resultado
     }
 
-    private fun ejecutaOperacionEnEspera() {
-        when(operacionEnEsperaDeOperando) {
-            "+" -> operando += operandoEnEspera
-            "-" -> operando = operandoEnEspera - operando
-            "*" -> operando *= operandoEnEspera
+    fun cambiarSigno():String {
+        resultado = if (resultado.startsWith("-")) {
+            resultado.replace("-", "")
+        } else {
+            "-$resultado"
+        }
+        return resultado
+    }
+
+    fun eliminarUltimaEntrada():String {
+        resultado = if ( resultado.replace("-", "").length > 1 ) {
+            resultado.substring(0, resultado.length - 1)
+        } else {
+            "0"
+        }
+        return resultado
+    }
+
+    fun operadorSeleccionado(nuevoOperador: String):String {
+        operacionEnEsperaDeOperando = nuevoOperador;
+        operando= resultado;
+        resultado = "0";
+        return resultado
+    }
+
+    fun calcularResultado(): String {
+        val number1: Double = operando.toDouble()
+        val number2: Double = resultado.toDouble()
+
+        when (operacionEnEsperaDeOperando) {
+            "+" -> resultado = "${number1 + number2}"
+            "-" -> resultado = "${number1 - number2}"
+            "*" -> resultado = "${number1 * number2}"
             "/" -> {
-                if (operando != 0) {
-                    operando = operandoEnEspera / operando
+                if (number2 != 0.0) {
+                    resultado = "${number1 / number2}"
                 }
             }
         }
+
+        operandoEnEspera = resultado
+
+        if (resultado.endsWith(".0")) {
+            resultado = resultado.substring(0, resultado.length - 2)
+        }
+
+        return resultado
     }
 
 }
