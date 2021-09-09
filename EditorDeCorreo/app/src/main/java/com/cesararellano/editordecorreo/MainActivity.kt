8c2ffab614440e08c93e22ed38b1eb6de9bc5c09
Cesar Arellano <cesar.arellano@gtec.com.mx>
2021-09-08 19:54:37 -0500

    Using Contracts

--- EditorDeCorreo/app/src/main/java/com/cesararellano/editordecorreo/MainActivity.kt
+++ EditorDeCorreo/app/src/main/java/com/cesararellano/editordecorreo/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.cesararellano.editordecorreo
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.TextView
+import androidx.activity.result.contract.ActivityResultContracts
 
 class MainActivity : AppCompatActivity() {
+    private val awaitResponse = registerForActivityResult( ActivityResultContracts.StartActivityForResult() ) {
+            resp ->
+        if( resp.resultCode == Activity.RESULT_OK ) {
+            val datos: Intent? = resp.data
+            findViewById<TextView>(R.id.labelEmail).text = datos?.getStringExtra("NEW_EMAIL")
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -14,11 +23,13 @@ class MainActivity : AppCompatActivity() {
 
     @Suppress( "UNUSED_PARAMETER")
     fun editEmail(botton: View) {
-        val campoDeTexto = findViewById<TextView>( R.id.labelEmail )
-        val correoActual = campoDeTexto.text.toString()
-        val intent = Intent(this, NuevoCorreoActivity::class.java).apply {
-            putExtra("CORREO_ACTUAL", correoActual)
-        }
-        startActivity( intent )
+        val labelEmail = findViewById<TextView>( R.id.labelEmail )
+        val currentEmail = labelEmail.text.toString()
+        //val intent = Intent(this, NuevoCorreoActivity::class.java).apply {
+        //    putExtra("CORREO_ACTUAL", correoActual)
+        //}
+        val intent = NuevoCorreoActivity.nuevaInstancia(this, currentEmail)
+        awaitResponse.launch(intent)
+        //startActivity( intent )
     }
 }
--- EditorDeCorreo/app/src/main/java/com/cesararellano/editordecorreo/NuevoCorreoActivity.kt
+++ EditorDeCorreo/app/src/main/java/com/cesararellano/editordecorreo/NuevoCorreoActivity.kt
@@ -1,14 +1,35 @@
 package com.cesararellano.editordecorreo
 
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.EditText
 
 class NuevoCorreoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_nuevo_correo)
-        val campoDeEdicion = findViewById<EditText>( R.id.editTextTextEmailAddress)
-        campoDeEdicion.setText( intent.getStringExtra("CORREO_ACTUAL"))
+        val emailEditText = findViewById<EditText>( R.id.emailEditText)
+        emailEditText.setText( intent.getStringExtra("CURRENT_EMAIL"))
+    }
+
+    fun ok( @Suppress("UNUSED_PARAMETER") botton: View ) {
+
+    }
+
+    fun cancel( @Suppress("UNUSED_PARAMETER") botton: View ) {
+        setResult(Activity.RESULT_CANCELED)
+        finish()
+    }
+
+    companion object {
+        fun nuevaInstancia(context: Context, currentEmail: String ): Intent {
+            return Intent(context, NuevoCorreoActivity::class.java).apply{
+                putExtra("CURRENT_EMAIL", currentEmail)
+            }
+        }
     }
 }
